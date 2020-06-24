82a22ee7566955362311f2219e0e83a5c1e503dc
rudy <rudolf.zachar@gmail.com>
2020-06-24 11:06:51 +0100

    test pub has name

---
+++ pub.rb
@@ -0,0 +1,11 @@
+class Pub
+
+    attr_reader :name, :drinks
+
+    def initialize(name, till, drinks)
+        @name = name
+        @till = till
+        @drinks = drinks
+    end
+
+end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -3,12 +3,20 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../pub')
+require_relative('../drink')
 
 class TestPub < Minitest::Test
 
     def setup()
-        # @pub = Pub.new("Rose n Crown", 1000, )
+        @drink1 = Drink.new("IPA", 5)
+        @drink2 = Drink.new("Pilsner", 6)
+        @drink3 = Drink.new("Lager", 4)
+        @drinks = [@drink1, @drink2, @drink3]
+        @pub = Pub.new("Rose n Crown", 1000, @drinks)
+    end
 
+    def test_pub_has_name()
+        assert_equal("Rose n Crown", @pub.name)
     end
 
 
