2caf465dc25e109844323671f6233e646781968a
rudy <rudolf.zachar@gmail.com>
2020-06-24 11:18:02 +0100

    test till has cash (true, and false) passed

--- pub.rb
+++ pub.rb
@@ -8,4 +8,12 @@ class Pub
         @drinks = drinks
     end
 
+    def cash_in_till?()
+        if @till > 0
+            return true
+        else
+            return false
+        end
+    end
+
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -13,11 +13,19 @@ class TestPub < Minitest::Test
         @drink3 = Drink.new("Lager", 4)
         @drinks = [@drink1, @drink2, @drink3]
         @pub = Pub.new("Rose n Crown", 1000, @drinks)
+        @pub2 = Pub.new("Brass Monkey", 0, @drinks)
     end
 
     def test_pub_has_name()
         assert_equal("Rose n Crown", @pub.name)
     end
 
+    def test_till_has_cash_true()
+        assert_equal(true, @pub.cash_in_till?())
+    end
+
+    def test_till_has_cash_false()
+        assert_equal(false, @pub2.cash_in_till?())
+    end
 
 end
